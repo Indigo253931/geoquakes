07eebfdd30eaa3721d82f1597a7f8bfacb9deaae
Indigo253931 <indigo253931@gmail.com>
2016-06-22 22:36:55 -0600

    solved step 3, still working on 4

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,36 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 var $quakesList;
 var map;
-var template;
+
+
+
+function initMap() {
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 39.76, lng: -105.01},
+		zoom: 1
+		}
+	);}
+
+		
+		
 
 $(document).on("ready", function() {
+	var source = $("#quakes-template").html();
+	var template= Handlebars.compile(source);
+
+	$.get(weekly_quakes_endpoint, function(data){
+
+		console.log(data);
+	
+		var quakeNames = template({quake:data.features});
+		  $("#info").append(quakeNames);
+
+
+	});
+
+	// $("#map").append(quakesList);
+
 
 
 });
